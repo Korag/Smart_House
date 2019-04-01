dfae0157261ec183081da9e8ea5dbc51964e83a6
Korag <koragrex@gmail.com>
2019-04-01 12:36:58 +0200

    Changed backend from ASP.NET MVC to ASP.NET MVC WebApi with individual Asp.Identity.

---
+++ SmartHouse_API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SmartHouse_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SmartHouse_API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SmartHouse_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
