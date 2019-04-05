c6f91d51aea8c96ba3f1ae53aa293d5b29c3f61a
Łukasz Czepielik <koragrex@gmail.com>
2019-04-06 00:08:02 +0200

    Added DbContext for the remote mlab MongoDB database environment with demanded credentials.

---
+++ SmartHouse_API/DAL/DbContext.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+
+namespace Certification_System.DAL
+{
+    public class DbContext
+    { 
+        private MongoClient _mongoClient;
+        public IMongoDatabase db;
+
+        public DbContext()
+        {
+            string _user = "Admin";
+            string _password = "************";
+            string _database = "smarthouse";
+
+            string _connectionstring = $"mongodb://{_user}:{_password}@ds040309.mlab.com:40309/{_database}";
+
+            _mongoClient = new MongoClient(_connectionstring);
+            db = _mongoClient.GetDatabase(_database);
+        }
+    }
+}
