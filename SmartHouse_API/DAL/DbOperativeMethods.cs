0eccd978234e3a1020cf8f4e85585a0a0a7016a0
Łukasz Czepielik <koragrex@gmail.com>
2019-04-06 00:08:59 +0200

    Added implementation of IDbOperative in DbOperativeMethods class which uses MongoDB context from DbContext class.

---
+++ SmartHouse_API/DAL/DbOperativeMethods.cs
@@ -0,0 +1,35 @@
+﻿using Certification_System.DAL;
+using MongoDB.Driver;
+using SmartHouse_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHouse_API.DAL
+{
+    public class DbOperativeMethods : IDbOperative
+    {
+        private DbContext _context;
+        private string _smartDeviceCollName = "SmartDevices";
+
+        private IMongoCollection<SmartDevice> _smartDevices { get; set; }
+
+        public DbOperativeMethods()
+        {
+            _context = new DbContext();
+        }
+
+        public void AddSmartDeviceToCollection(SmartDevice device)
+        {
+            List<SmartDevice> devices = GetSmartDevicesCollection().ToList();
+            devices.Add(device);
+        }
+
+        public IEnumerable<SmartDevice> GetSmartDevicesCollection()
+        {
+            _smartDevices = _context.db.GetCollection<SmartDevice>(_smartDeviceCollName);
+            return _smartDevices.AsQueryable().ToList();
+        }
+    }
+}
