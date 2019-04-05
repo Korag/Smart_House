ace4ea26882a4e368e957a6bcbdab356cda8a007
Łukasz Czepielik <koragrex@gmail.com>
2019-04-06 00:06:07 +0200

    Added SmartDevice model (first attempt to store in appropriate way).

---
+++ SmartHouse_API/Models/SmartDevice.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SmartHouse_API.Models
+{
+    public class SmartDevice
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Localization { get; set; }
+        public bool Disabled { get; set; }
+    }
+}
