16eacaad25ed3375eb9cba6e5c8f11379327f128
Łukasz Czepielik <koragrex@gmail.com>
2019-04-06 00:07:19 +0200

    Added interface IDbOperative for the operations which depends from Mongo Database.

---
+++ SmartHouse_API/DAL/IDbOperative.cs
@@ -0,0 +1,12 @@
+﻿using SmartHouse_API.Models;
+using System.Collections.Generic;
+
+namespace SmartHouse_API.DAL
+{
+    public interface IDbOperative
+    {
+        IEnumerable<SmartDevice> GetSmartDevicesCollection();
+        void AddSmartDeviceToCollection(SmartDevice device);
+
+    }
+}
