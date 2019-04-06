215c77f229ddbc0319840de754465516e1dc3f0d
Łukasz Czepielik <koragrex@gmail.com>
2019-04-06 16:57:59 +0200

    Added SmartDeviceState class with appropriate enum inside.

---
+++ SmartHouse_API/Models/SmartDeviceState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHouse_API.Models
+{
+    public enum State
+    {
+        state1,
+        state2,
+        state3,
+        state4,
+        state5
+    }
+}
