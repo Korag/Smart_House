1878b274be2d256ae990c5d1c58af7992a15d69f
Łukasz Czepielik <koragrex@gmail.com>
2019-04-06 16:57:48 +0200

    Added SmartDeviceLocalization class with appropriate enum inside.

---
+++ SmartHouse_API/Models/SmartDeviceLocalization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHouse_API.Models
+{
+    public enum Localization
+    {
+        loc1,
+        loc2,
+        loc3,
+        loc4
+    }
+}
