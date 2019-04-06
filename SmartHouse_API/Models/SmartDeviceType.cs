d4a60e2b4f1a640a7ec67985258302e78baa265b
Łukasz Czepielik <koragrex@gmail.com>
2019-04-06 16:58:51 +0200

    Added SmartDeviceType enum.

---
+++ SmartHouse_API/Models/SmartDeviceType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHouse_API.Models
+{
+    public enum Type
+    {
+        type1,
+        type2,
+        type3,
+        type4
+    }
+}
