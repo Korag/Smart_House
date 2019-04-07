fff53552be71c5585b5fca72729c4dc7c23efe71
Łukasz Czepielik <koragrex@gmail.com>
2019-04-07 19:49:45 +0200

    Added SmartDevicesController with method GetAllSmartDevices which returns List downloaded from MongoDB of SmartDevice types.

---
+++ SmartHouse_API/Controllers/SmartDevicesController.cs
@@ -0,0 +1,27 @@
+﻿using SmartHouse_API.DAL;
+using SmartHouse_API.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SmartHouse_API.Controllers
+{
+    public class SmartDevicesController : Controller
+    {
+        private IDbOperative _context;
+
+        public SmartDevicesController()
+        {
+            _context = new DbOperativeMethods();
+        }
+
+        [HttpGet]
+        public List<SmartDevice> GetAllSmartDevices()
+        {
+            List<SmartDevice> SmartDevicesList = _context.GetSmartDevicesCollection().ToList();
+            return SmartDevicesList;
+        }
+
+
+    }
+}
