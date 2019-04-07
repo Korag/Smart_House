73fb344e36caff99a5842a56f1d043d4b17dddf6
Kamil Hareza <kml.hareza@gmail.com>
2019-04-07 22:26:57 +0200

    Added device model to unity project

---
+++ SmartHouse_Unity/Assets/Scripts/DeviceModel.cs
@@ -0,0 +1,9 @@
+﻿public class DeviceModel
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string State { get; set; }
+    public string Localization { get; set; }
+    public bool Disabled { get; set; }
+}
