4d38c0d5df688d28852ae9b8caa2f4fc3585bde1
Kamil Hareza <kml.hareza@gmail.com>
2019-04-07 22:24:01 +0200

    Added JsonHelper script and basic logic to api connection script

--- SmartHouse_Unity/Assets/Scripts/ApiConnection.cs
+++ SmartHouse_Unity/Assets/Scripts/ApiConnection.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ApiConnection : MonoBehaviour
 {
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator GetDevices()
     {
-        
+        string getmartDevicesApiUrl = "Api_url_here";
+        using (UnityWebRequest www = UnityWebRequest.Get(getmartDevicesApiUrl))
+        {
+            //www.chunkedTransfer = false;
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                if (www.isDone)
+                {
+                    string jsonResult =
+                        System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                    Debug.Log(jsonResult);
+
+                }
+                //ddlCountries.options.AddRange(entities.
+            }
+        }
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+
     }
 }
