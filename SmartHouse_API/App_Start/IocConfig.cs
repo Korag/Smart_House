55d6c4b64e857b8eb928fa7b84ef761ab0393d1d
Kamil Hareza <kml.hareza@gmail.com>
2019-04-08 23:31:03 +0200

    Added IocConfig class with autofac configure method

---
+++ SmartHouse_API/App_Start/IocConfig.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using System.Web.Http;
+
+namespace SmartHouse_API.App_Start
+{
+    public class IocConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        }
+    }
+}
