3794cb8f11e6480e2a413c9ffc80c915eaa1b0cd
Kamil Hareza <kml.hareza@gmail.com>
2019-04-14 17:33:27 +0200

    Added basic arrow controls to camera control script

--- SmartHouse_Unity/Assets/Scripts/CameraControl.cs
+++ SmartHouse_Unity/Assets/Scripts/CameraControl.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Move : MonoBehaviour
+public class CameraControl : MonoBehaviour
 {
+    public float speed = 5f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
     }
 }
