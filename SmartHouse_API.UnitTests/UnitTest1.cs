b2ca07b040672b43d59409abf39623ac11fb2fed
Kamil Hareza <kml.hareza@gmail.com>
2019-04-14 18:06:53 +0200

    Added new project in order to test the SmartHouse_API

---
+++ SmartHouse_API.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartHouse_API.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
