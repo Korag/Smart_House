d2e67dae8e4a24d900f25587cbc5cb95d462b036
Kamil Hareza <kml.hareza@gmail.com>
2019-04-14 18:20:20 +0200

    Added moq package and 3 blank tests

---
+++ SmartHouse_API.UnitTests/SmartDevicesControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace SmartHouse_API.UnitTests
+{
+    [TestFixture]
+    public class SmartDevicesControllerTests
+    {
+        [Test]
+        public void CheckIfSingleSmartDeviceIsDisabled_WhenThereIsADisabledDevice_ReturnItsState()
+        {
+
+        }
+
+        [Test]
+        public void CheckIfSingleSmartDeviceIsDisabled_WhenThereIsNotADisabledDevice_ReturnItsState()
+        {
+
+        }
+
+        [Test]
+        public void CheckIfSingleSmartDeviceIsDisabled_WhenIdIsWrong_ThrowException()
+        {
+
+        }
+    }
+}
