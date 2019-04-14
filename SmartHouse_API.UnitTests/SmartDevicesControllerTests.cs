f15bc3028cd6adffd43f1f6bed4a51cdc369008f
Kamil Hareza <kml.hareza@gmail.com>
2019-04-14 18:58:42 +0200

    Added 3 tests for CheckIfSingleSmartDeviceIsDisabled method

--- SmartHouse_API.UnitTests/SmartDevicesControllerTests.cs
+++ SmartHouse_API.UnitTests/SmartDevicesControllerTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using MongoDB.Bson;
+using Moq;
+using NUnit.Framework;
+using SmartHouse_API.Controllers;
+using SmartHouse_API.DAL;
 
 namespace SmartHouse_API.UnitTests
 {
@@ -8,19 +12,48 @@ namespace SmartHouse_API.UnitTests
         [Test]
         public void CheckIfSingleSmartDeviceIsDisabled_WhenThereIsADisabledDevice_ReturnItsState()
         {
+            var deviceId = ObjectId.Parse("5caa438add8c6316e4b491cc");
 
+            var contextDbMock = new Mock<IDbOperative>();
+            contextDbMock.Setup(x => x.GetSingleSmartDeviceFromCollection(deviceId))
+                .Returns(new Models.SmartDevice { Id = deviceId, Name = "DeviceName", Disabled = true });
+
+            SmartDevicesController controller = new SmartDevicesController(contextDbMock.Object);
+
+            var result = controller.CheckIfSingleSmartDeviceIsDisabled(deviceId.ToString());
+
+            Assert.That(result, Is.EqualTo(true));
         }
 
         [Test]
         public void CheckIfSingleSmartDeviceIsDisabled_WhenThereIsNotADisabledDevice_ReturnItsState()
         {
+            var deviceId = ObjectId.Parse("5caa438add8c6316e4b491cc");
 
+            var contextDbMock = new Mock<IDbOperative>();
+            contextDbMock.Setup(x => x.GetSingleSmartDeviceFromCollection(deviceId))
+                .Returns(new Models.SmartDevice { Id = deviceId, Name = "DeviceName", Disabled = false });
+
+            SmartDevicesController controller = new SmartDevicesController(contextDbMock.Object);
+
+            var result = controller.CheckIfSingleSmartDeviceIsDisabled(deviceId.ToString());
+
+            Assert.That(result, Is.EqualTo(false));
         }
 
         [Test]
         public void CheckIfSingleSmartDeviceIsDisabled_WhenIdIsWrong_ThrowException()
         {
+            var deviceId = ObjectId.Parse("5caa438add8c6316e4b491cd");
 
+            var contextDbMock = new Mock<IDbOperative>();
+            contextDbMock.Setup(x => x.GetSingleSmartDeviceFromCollection(new ObjectId("5dda438add8c6316e4b491cc")))
+                .Returns(new Models.SmartDevice { Id = new ObjectId("5dda438add8c6316e4b491cc"), Name = "DeviceName", Disabled = true });
+
+            SmartDevicesController controller = new SmartDevicesController(contextDbMock.Object);
+
+            Assert.That(() => controller.CheckIfSingleSmartDeviceIsDisabled(deviceId.ToString()),
+                Throws.TypeOf<System.NullReferenceException>());
         }
     }
 }
