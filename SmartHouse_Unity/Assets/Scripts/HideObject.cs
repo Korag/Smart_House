a3c431a16c77a20e87311668b26432f9f2115347
Kamil Hareza <kml.hareza@gmail.com>
2019-04-21 16:32:45 +0200

    Added main logic to HideObject script, now its hiding object when camera is close enough

--- SmartHouse_Unity/Assets/Scripts/HideObject.cs
+++ SmartHouse_Unity/Assets/Scripts/HideObject.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class HideObject : MonoBehaviour
 {
+    public Transform camera;
+    public float HideDistance = 50f;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        float dist = Vector3.Distance(camera.position, transform.position);
+        if (dist <= HideDistance)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+
+        }
+
     }
 }
