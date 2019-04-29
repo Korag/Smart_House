a4bd51e11124d497f32cfd7cd04a06c56d7be37f
Kamil Hareza <kml.hareza@gmail.com>
2019-04-29 16:23:53 +0200

    Fixed camera zoom

--- SmartHouse_Unity/Assets/Scripts/CameraController.cs
+++ SmartHouse_Unity/Assets/Scripts/CameraController.cs
@@ -6,17 +6,10 @@ public class CameraController : MonoBehaviour
     public float speed = 5f;
     public float rotationSpeed = 5f;
     public float borderDistance = 10;
-    public Vector2 panLimit;
-
     public float scrollSpeed = 20f;
-    public float minY = 20f;
-    public float maxY = 120f;
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
-    // Update is called once per frame
     private void Update()
     {
 
@@ -32,31 +25,20 @@ public class CameraController : MonoBehaviour
         }
         if (Input.GetKey("a") || Input.mousePosition.x <= borderDistance)
         {
-            pos.x -= speed * Time.deltaTime;
+            pos -= transform.right * (speed * Time.deltaTime);
         }
         if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - borderDistance)
         {
-            pos.x += speed * Time.deltaTime;
+            pos += transform.right * (speed * Time.deltaTime);
         }
 
         //camera rotation
         if (Input.GetKey(KeyCode.Mouse1))
         {
             var yaw = transform.eulerAngles.y + speedH * Input.GetAxis("Mouse X");
-            //pitch -= speedV * Input.GetAxis("Mouse Y");
 
             transform.eulerAngles = new Vector3(0, yaw, 0.0f);
         }
-
-
-
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
-
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
-
 
         transform.position = pos;
     }
---
+++ SmartHouse_Unity/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float minY = 20f;
+    public float maxY = 120f;
+    public Vector2 panLimit;
+    public Transform ground;
+
+    public float scrollSpeed = 20f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var groundY = ground.transform.position.y;
+        Vector3 pos = transform.position;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos += ground.forward * scroll * scrollSpeed * 100f * Time.deltaTime;
+        pos.y += groundY - scroll * scrollSpeed * 100f * Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+
+
+        transform.position = pos;
+
+    }
+}
