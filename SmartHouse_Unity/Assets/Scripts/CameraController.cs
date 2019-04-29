970d87340411b008732d4bb2efac94d5e1cca850
Kamil Hareza <kml.hareza@gmail.com>
2019-04-29 15:49:26 +0200

    Added camera rotation and fixed camera movement

--- SmartHouse_Unity/Assets/Scripts/CameraController.cs
+++ SmartHouse_Unity/Assets/Scripts/CameraController.cs
@@ -4,26 +4,31 @@ public class CameraController : MonoBehaviour
 {
 
     public float speed = 5f;
+    public float rotationSpeed = 5f;
     public float borderDistance = 10;
     public Vector2 panLimit;
 
     public float scrollSpeed = 20f;
     public float minY = 20f;
     public float maxY = 120f;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
 
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
     // Update is called once per frame
     private void Update()
     {
 
         Vector3 pos = transform.position;
-
+        //camera movement
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - borderDistance)
         {
-            pos.z += speed * Time.deltaTime;
+            pos += transform.forward * (speed * Time.deltaTime);
         }
         if (Input.GetKey("s") || Input.mousePosition.y <= borderDistance)
         {
-            pos.z -= speed * Time.deltaTime;
+            pos -= transform.forward * (speed * Time.deltaTime);
         }
         if (Input.GetKey("a") || Input.mousePosition.x <= borderDistance)
         {
@@ -33,6 +38,16 @@ public class CameraController : MonoBehaviour
         {
             pos.x += speed * Time.deltaTime;
         }
+
+        //camera rotation
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            var yaw = transform.eulerAngles.y + speedH * Input.GetAxis("Mouse X");
+            //pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(0, yaw, 0.0f);
+        }
+
 
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
