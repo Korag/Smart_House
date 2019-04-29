bc40ae6b39f02a9912de8ae91012f32c6be34650
Kamil Hareza <kml.hareza@gmail.com>
2019-04-29 17:46:21 +0200

    Added scripts that enables clicking on objects and displaying thier names

---
+++ SmartHouse_Unity/Assets/Scripts/ActionSelector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ActionSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                if (hit.transform != null)
+                {
+                    DisplayListOfActions(hit.transform.gameObject);
+
+                }
+
+            }
+        }
+    }
+
+    private void DisplayListOfActions(GameObject device)
+    {
+        print(device.name);
+    }
+}
