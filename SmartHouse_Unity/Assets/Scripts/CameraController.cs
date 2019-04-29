9afc997606e448446f5377e24f9f5862c09d37ec
Kamil Hareza <kml.hareza@gmail.com>
2019-04-29 16:57:11 +0200

    Added camera movement using mouse

--- SmartHouse_Unity/Assets/Scripts/CameraController.cs
+++ SmartHouse_Unity/Assets/Scripts/CameraController.cs
@@ -3,41 +3,54 @@
 public class CameraController : MonoBehaviour
 {
 
-    public float speed = 5f;
+    public float speed = 3f;
     public float rotationSpeed = 5f;
     public float borderDistance = 10;
     public float scrollSpeed = 20f;
     public float speedH = 2.0f;
-    public float speedV = 2.0f;
+    private float yRotation;
+    private float xRotation;
+    private void start()
+    {
+        yRotation = transform.eulerAngles.y;
+        xRotation = transform.eulerAngles.x;
 
+    }
     private void Update()
     {
-
         Vector3 pos = transform.position;
-        //camera movement
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - borderDistance)
+        //camera movement using keyes
+        if (Input.GetKey("w"))
         {
-            pos += transform.forward * (speed * Time.deltaTime);
+            pos += transform.forward * (speed * 10 * Time.deltaTime);
         }
-        if (Input.GetKey("s") || Input.mousePosition.y <= borderDistance)
+        if (Input.GetKey("s"))
         {
-            pos -= transform.forward * (speed * Time.deltaTime);
+            pos -= transform.forward * (speed * 10 * Time.deltaTime);
         }
-        if (Input.GetKey("a") || Input.mousePosition.x <= borderDistance)
+        if (Input.GetKey("a"))
         {
-            pos -= transform.right * (speed * Time.deltaTime);
+            pos -= transform.right * (speed * 10 * Time.deltaTime);
         }
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - borderDistance)
+        if (Input.GetKey("d"))
         {
-            pos += transform.right * (speed * Time.deltaTime);
+            pos += transform.right * (speed * 10 * Time.deltaTime);
         }
 
-        //camera rotation
+
+        //camera movement using mpuse
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            pos -= transform.right * (speed * Input.GetAxis("Mouse X"));
+            pos -= transform.forward * (speed * Input.GetAxis("Mouse Y"));
+        }
+
+        //camera rotation by X axis
         if (Input.GetKey(KeyCode.Mouse1))
         {
-            var yaw = transform.eulerAngles.y + speedH * Input.GetAxis("Mouse X");
+            yRotation = transform.eulerAngles.y + speedH * Input.GetAxis("Mouse X");
 
-            transform.eulerAngles = new Vector3(0, yaw, 0.0f);
+            transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
         }
 
         transform.position = pos;
