13bdc7f77200ae85ffa250e069cf9a7aad48b58d
Ownerro <ziom19962@gmail.com>
2019-05-05 15:40:27 +0200

    Added menu component with simple styles

--- smart_house_site/src/App.vue
+++ smart_house_site/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu></Menu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Menu
   }
 }
 </script>
