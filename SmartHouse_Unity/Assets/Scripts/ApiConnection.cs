173ed555276470fecb49ae8cf664208629be7221
Kamil Hareza <kml.hareza@gmail.com>
2019-05-05 21:56:24 +0200

    Added device state script which downloads state from database

--- SmartHouse_Unity/Assets/Scripts/ApiConnection.cs
+++ SmartHouse_Unity/Assets/Scripts/ApiConnection.cs
@@ -20,4 +20,21 @@ public class ApiConnection
 
         return device.AvailableActions;
     }
+
+    public string GetDeviceState(string deviceName)
+    {
+        string getmartDevicesApiUrl = "http://localhost:61635/api/GetAllSmartDevices";
+
+        HttpWebRequest request =
+          (HttpWebRequest)WebRequest.Create(getmartDevicesApiUrl);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+
+        string jsonResponse = "{\"Items\":" + reader.ReadToEnd() + "}";
+        var resultFromDataBase = JsonHelper.FromJson<DeviceModel>(jsonResponse);
+        var device = resultFromDataBase.Where(x => x.Type == deviceName).First();
+
+        return device.State;
+    }
+
 }
--- SmartHouse_Unity/Assets/Scripts/DeviceState.cs
+++ SmartHouse_Unity/Assets/Scripts/DeviceState.cs
@@ -1,28 +1,88 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DeviceState : MonoBehaviour
 {
-    public string CurrentState = "Active";
+    public string CurrentState = "On";
     public List<GameObject> StateIcons;
 
     private GameObject ActiveDeviceState;
-    private GameObject test;
+    private GameObject newState;
+
+
+    private ApiConnection api = new ApiConnection();
 
     private float deviceStatePositionY;
-
     private void Start()
     {
-        //deviceStatePositionY = transform.position.y + transform.lossyScale.y + 50f;
-        //ActiveDeviceState = Instantiate(StateIcons[0],
-        //    new Vector3(transform.position.x, deviceStatePositionY, transform.position.z),
-        //    Quaternion.identity);
-        InvokeRepeating("UpdateDeviceState", 1f, 1f);  //1s delay, repeat every 1s
+        ActiveDeviceState = GameObject.Find("DeviceState");
+
+        SetDeviceState();
+
+        InvokeRepeating("UpdateDeviceState", 0f, 5f);  //1s delay, repeat every 1s
+        //InvokeRepeating("SetDeviceState", 1f, 1f);  //1s delay, repeat every 1s
     }
 
     private void UpdateDeviceState()
     {
+        CurrentState = api.GetDeviceState(transform.name);
+        ChangeDeviceState();
         Debug.Log("Update");
+        Debug.Log(CurrentState);
+    }
+
+    private void ChangeDeviceState()
+    {
+        DestroyAllChildObjects();
+        if (ActiveDeviceState.transform.childCount == 0)
+        {
+            var stateFromList = StateIcons.Where(x => x.name == CurrentState).FirstOrDefault();
+            GameObject stateToRender;
+            if (stateFromList != null)
+            {
+                stateToRender = stateFromList;
+            }
+            else
+            {
+                stateToRender = StateIcons[1];
+            }
+            var newObject = Instantiate(stateToRender);
+            newObject.transform.parent = ActiveDeviceState.transform;
+        }
+
+    }
+
+    private void SetDeviceState()
+    {
+        if (ActiveDeviceState.transform.childCount == 0)
+        {
+            var stateFromList = StateIcons.Where(x => x.name == CurrentState).FirstOrDefault();
+            foreach (var item in StateIcons)
+            {
+                print(item.name);
+            }
+            GameObject stateToRender;
+            if (stateFromList != null)
+            {
+                stateToRender = stateFromList;
+            }
+            else
+            {
+                stateToRender = StateIcons[1];
+            }
+            var newObject = Instantiate(stateToRender);
+            newObject.transform.parent = ActiveDeviceState.transform;
+        }
+    }
+
+    private void DestroyAllChildObjects()
+    {
+        foreach (Transform child in ActiveDeviceState.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        print("Destroy");
     }
 
     // Update is called once per frame
@@ -30,7 +90,5 @@ public class DeviceState : MonoBehaviour
     {
 
     }
-
-
 
 }
