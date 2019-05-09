491f6410d40a34cc7816bce353f0357f6fb21a00
Łukasz Czepielik <koragrex@gmail.com>
2019-05-10 01:11:38 +0200

    Added Localization Dictionary collection Model.

---
+++ SmartHouse_API/Models/Localization.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SmartHouse_API.Models
+{
+    public class Localization
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+    }
+}
