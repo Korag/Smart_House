2d45933c30d363d09252ea6fc832a380c77b45e5
Ownerro <ziom19962@gmail.com>
2019-05-12 21:04:36 +0200

    Added display logic for Menu

--- smart_house_site/src/components/Menu.vue
+++ smart_house_site/src/components/Menu.vue
@@ -1,8 +1,8 @@
 <template>
-    <div id="menu">
-        <div v-on:click="test" class="menu" v-for="option in menu" v-bind:key="option.id">
+    <div v-show="this.$store.getters.getDisplayStatusForMenu">
+        <div v-on:click="test" class="menuItem" v-for="option in menu" v-bind:key="option.id">
             <img :src=option.image>
-            <div class="menuText">{{option.name}}</div>
+            <div>{{option.name}}</div>
         </div>
     </div>
 </template>
@@ -16,6 +16,8 @@ export default {
     },
     methods:{
         test: function(){
+            this.$store.commit('displayMenu');
+            this.$store.commit('displayListOfDevices');
             this.$store.dispatch('getDevices');
         }
     }
@@ -23,19 +25,30 @@ export default {
 </script>
 
 <style scoped>
-.menu {
-    border: 2px solid blue;
-    border-radius: 5px;
-    height: 45vh;
-    width: 45vw;
+.menuItem {
+    height: 20vh;
+    width: 46vw;
+    border-style: solid;
+    border-color: white;
+    border-width: 2vw;
     float: left;
-    margin: 1vh 1vh;
     position: relative;
+    background: #166fff;
+    font-size: 30px;
+    text-align: center;
+    color: white;
 }
 .menuText{
     position: absolute;
     bottom: 10vh;
     right: 15vw;
-    
 }
+.topBar{
+    background: #166fff;
+    height: 5vh;
+    text-align: center;
+    color: white;
+    font-size: 30px;
+}
+
 </style>
