b5b075e13dfd97761ba991085274dc54798c1006
Ownerro <ziom19962@gmail.com>
2019-05-12 21:02:58 +0200

    Added TopBar component

--- smart_house_site/src/App.vue
+++ smart_house_site/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <TopBar></TopBar>
     <Menu></Menu>
     <DevicesList></DevicesList>
   </div>
@@ -7,25 +8,33 @@
 
 <script>
 import Menu from './components/Menu.vue';
-import DevicesList from './components/DevicesList.vue'
+import DevicesList from './components/DevicesList.vue';
+import TopBar from './components/TopBar.vue'
 
 
 export default {
   name: 'app',
   components: {
     Menu,
-    DevicesList
+    DevicesList,
+    TopBar
   }
 }
 </script>
 
 <style>
+
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ smart_house_site/src/components/TopBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="topBar" v-on:click="goBack" >
+        <img v-if="!this.$store.getters.getDisplayStatusForMenu" class="arrow" :src="require('@/assets/arrow.svg')">
+        Smart House</div>
+</template>
+
+<script>
+export default {
+    methods:{
+        goBack: function(){
+            this.$store.commit('displayListOfDevices');
+            this.$store.commit('displayMenu');
+        }
+    }
+}
+</script>
+
+<style scoped>
+.topBar{
+    background: #166fff;
+    height: 5vh;
+    text-align: center;
+    color: white;
+    font-size: 30px;
+}
+.arrow{
+    float: left;
+}
+</style>
