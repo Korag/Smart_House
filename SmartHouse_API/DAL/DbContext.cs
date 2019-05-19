e036fff20a80d11bf64c0df2fd9ec615d42274eb
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 17:03:37 +0200

    Changed DbContext for documentation purpouses SwaggerUI.

--- SmartHouse_API/DAL/DbContext.cs
+++ SmartHouse_API/DAL/DbContext.cs
@@ -10,10 +10,10 @@ namespace SmartHouse_API.DAL
         public DbContext()
         {
             string _user = "Admin";
-            string _password = "vg8Jj6GPhnqS8Kf";
-            string _database = "smarthouse";
+            string _password = "admin123";
+            string _database = "smarthouse_swagger";
 
-            string _connectionstring = $"mongodb://{_user}:{_password}@ds040309.mlab.com:40309/{_database}";
+            string _connectionstring = $"mongodb://{_user}:{_password}@ds038547.mlab.com:38547/{_database}";
 
             _mongoClient = new MongoClient(_connectionstring);
             db = _mongoClient.GetDatabase(_database);
