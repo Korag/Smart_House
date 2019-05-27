2ca3362529e1ab5387e74d706892e9189bfc48f3
Ownerro <ziom19962@gmail.com>
2019-05-27 11:04:11 +0200

    New styles for Menu

--- smart_house_site/src/components/Menu.vue
+++ smart_house_site/src/components/Menu.vue
@@ -1,10 +1,14 @@
 <template>
-    <div v-show="this.$store.getters.getDisplayStatus.showMenu">
-        <div v-on:click="showDevicesList" class="menuItem" v-for="option in menu" v-bind:key="option.id">
-            <img :src=option.image>
-            <div>{{option.name}}</div>
-        </div>
-    </div>
+    <v-layout row wrap v-show="this.$store.getters.getDisplayStatus.showMenu">
+        <v-flex px-3 py-2 xs12 sm6 lg4 v-for ="option in menu" :key="option.id" v-on:click="showDevicesList">
+            <v-card class="text-xs-center white--text headline" color="blue">
+                <v-icon size="100px" color="white">{{option.icon}}</v-icon>
+                <v-card-text>
+                {{option.name}}
+                </v-card-text>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
