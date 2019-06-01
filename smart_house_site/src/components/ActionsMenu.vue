ecd6170fc2d5410bb69b461c6bc98e2f1390d452
Ownerro <ziom19962@gmail.com>
2019-06-02 01:48:14 +0200

    Updated component ActionsMenu

--- smart_house_site/src/components/ActionsMenu.vue
+++ smart_house_site/src/components/ActionsMenu.vue
@@ -1,29 +1,37 @@
 <template>
-    <div v-if="this.$store.getters.getDisplayStatus.showActionsList">
-        <div class="actualState">Aktualny stan: {{actualState}}</div>
-        <div class="menuItem" v-for="action in actions
-            .filter((e)=>{
-            return e !=actualState})"
-            v-bind:key="action"
-            v-on:click='changeState(action)'>
-            <div>{{action}}</div>
-        </div>
-    </div>
+    <v-container v-if="this.$store.getters.getDisplayStatus.showActionsList">
+        <v-layout>
+            <v-flex>
+                <v-card flat color="grey lighten-3" class="text-xs-center">
+                    <div>Aktualny stan: {{actualDevice.State}}</div>
+                <v-card-actions>
+                    <v-layout>
+                        <v-flex v-for="action in listOfActions(actualDevice)" :key="action">
+                            <v-btn>{{action}}</v-btn>
+                        </v-flex>
+                    </v-layout>
+                </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
 export default {
     computed:{
-        actions(){
-            return this.$store.getters.getActionsForDevice;
-        },
-        actualState(){
-            return this.$store.getters.getActualDeviceState;
+        actualDevice(){
+            return this.$store.getters.getActualDevice;
         }
     },
     methods:{
         changeState(newState){
             this.$store.dispatch('changeDeviceState',newState);
+        },
+        listOfActions(actualDevice){
+            return this.$store.getters.getListOfActionForDevice(actualDevice).filter((ele)=>{
+                return ele != actualDevice.State;
+            });;
         }
     }
 }
