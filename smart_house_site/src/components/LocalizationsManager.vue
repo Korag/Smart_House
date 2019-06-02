e2b425bd485247de43e591bdb4db4555b18c86d2
Ownerro <ziom19962@gmail.com>
2019-06-02 18:45:53 +0200

    Added deleteLocalizationFromDB

--- smart_house_site/src/components/LocalizationsManager.vue
+++ smart_house_site/src/components/LocalizationsManager.vue
@@ -4,18 +4,21 @@
         <v-flex py-1 v-for ="localization in localizationList" :key="localization.Id" >
             <v-card flat class="blue white--text">
                 <v-layout row wrap text-xs-center >
-                    <v-flex lg4 sm12 xs12 px-4 >
+                    <v-flex xs12 sm12 md1 lg1 xl1>
+                        <v-icon size="60px" color="white" left>{{localization.Icon}}</v-icon>
+                    </v-flex>
+                    <v-flex  xs12 sm12 md3 lg3 xl1 offset-lg3 offset-xl4>
                         <div class="black--text caption">Localization Name</div>
                         <div class="headline">{{localization.Name}}</div>
                     </v-flex>
-                    <v-flex sm12 md12 lg2 offset-lg2  >
+                    <v-flex sm12 md1 lg1 xl1 offset-lg3 offset-md6 offset-xl5>
                         <v-btn large color="error" v-on:click="deleteLocalizationFromDB(localization)">Delete</v-btn>
                     </v-flex>
                 </v-layout>
             </v-card>
         </v-flex>
-              <v-btn large  bottom right fixed fab color="green" v-on:click="showAddDevice">
-                <v-icon white>add</v-icon>
+              <v-btn large  bottom right fixed fab color="green" v-on:click="showAddLocalization">
+                <v-icon color="white">add</v-icon>
               </v-btn>
     </v-layout>
 </v-container>
@@ -33,19 +36,11 @@ export default {
         }
     },
     methods:{
-        showAddDevice(){
-            Promise.all([this.$store.dispatch('getDevicesTypes'),this.$store.dispatch('getLocalizations')]).then(()=>{
-                this.$store.commit('display',{to:'showAddDevice',from:'showDevicesManager'});
-            })
-        },
         deleteLocalizationFromDB(localization){
             this.$store.dispatch('deleteLocalizationFromDB',localization);
         },
-        showEditDevice(device){
-            Promise.all([this.$store.dispatch('getDevicesTypes'),this.$store.dispatch('getLocalizations')]).then(()=>{
-                this.$store.commit('changeActualDevice',device);
-                this.$store.commit('display',{to:'showEditDevice',from:'showDevicesManager'});
-            })
+        showAddLocalization(device){
+                this.$store.commit('display',{to:'showAddLocalization',from:'showLocalizationsManager'});
         }
     }
 }
