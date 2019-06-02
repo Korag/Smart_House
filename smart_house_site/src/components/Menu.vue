e324adfdcc4a146d61e8ec447e7a7b1b96dcbe1b
Ownerro <ziom19962@gmail.com>
2019-06-02 03:07:38 +0200

    Added useFunction and showAddDevice

--- smart_house_site/src/components/Menu.vue
+++ smart_house_site/src/components/Menu.vue
@@ -1,8 +1,8 @@
 <template>
 <v-container v-show="this.$store.getters.getDisplayStatus.showMenu">
     <v-layout row wrap v-show="this.$store.getters.getDisplayStatus.showMenu">
-        <v-flex px-3 py-2 xs12 sm6 lg4 v-for ="option in menu" :key="option.id" v-on:click="showDevicesList">
-            <v-card class="text-xs-center white--text headline" color="blue">
+        <v-flex px-3 py-2 xs12 sm6 lg4 v-for ="option in menu" :key="option.id" >
+            <v-card class="text-xs-center white--text headline" color="blue" v-on:click="useFunction(option.action)">
                 <v-icon size="100px" color="white">{{option.icon}}</v-icon>
                 <v-card-text>
                 {{option.name}}
@@ -21,11 +21,23 @@ export default {
         }
     },
     methods:{
+        useFunction(name){
+            if(name == 'showDevicesList'){
+                this.showDevicesList();
+            }
+            else if(name == 'showAddDevice'){
+                this.showAddDevice();
+            }
+        },
         showDevicesList: function(){
             Promise.all([this.$store.dispatch('getDevices'),this.$store.dispatch('getLocalizations'),this.$store.dispatch('getActions')]).then(()=>{
                 this.$store.commit('createGroups');
                  this.$store.commit('display',{to:'showDevicesList',from:'showMenu'});
             })
+        },
+        showAddDevice: function(){
+            this.$store.dispatch('getDevicesTypes');
+            this.$store.commit('display',{to:'showAddDevice',from:'showMenu'});
         }
     }
 }
