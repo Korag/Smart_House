733a703b5ea46bb6336ac2f6a3a71accaf27e54a
khareza <kml.hareza@gmail.com>
2019-06-02 14:00:26 +0200

    Added acceptance test that checks get all smart devices request

---
+++ SmartHouse_API.AcceptanceTests/Steps/SmartDevicesControllerSteps.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using SmartHouse_API.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SmartHouse_API.AcceptanceTests.Steps
+{
+    [Binding]
+    public class SmartDevicesControllerSteps
+    {
+        private HttpClient _client = new HttpClient();
+        private HttpResponseMessage _responseContent;
+        private List<SmartDevice> _result;
+
+
+        [When(@"Client sends get request in order to get all smart devices")]
+        public void WhenClientSendsGetRequestInOrderToGetAllSmartDevices()
+        {
+            _responseContent = _client.GetAsync("https://smarthouseapii.azurewebsites.net/api/GetAllSmartDevices").Result;
+            _result = JsonConvert.DeserializeObject<List<SmartDevice>>(_responseContent.Content.ReadAsStringAsync().Result);
+        }
+
+        [Then(@"The client should not get empty list")]
+        public void ThenTheClientShouldNotGetEmptyList()
+        {
+            Assert.That(_result, Is.Not.Empty);
+        }
+
+        [Then(@"The client should get list of smart devices")]
+        public void ThenTheClientShouldGetListOfSmartDevices()
+        {
+            Assert.That(_result, Is.TypeOf<List<SmartDevice>>());
+        }
+
+    }
+}
--- SmartHouse_API/Models/SmartDevice.cs
+++ SmartHouse_API/Models/SmartDevice.cs
@@ -1,5 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace SmartHouse_API.Models
@@ -7,6 +9,7 @@ namespace SmartHouse_API.Models
     public class SmartDevice
     {
         [BsonId]
+        [JsonConverter(typeof(ObjectIdConverter))]
         public ObjectId Id { get; set; }
         public string Type { get; set; }
         public string Name { get; set; }
@@ -14,5 +17,38 @@ namespace SmartHouse_API.Models
         public bool Disabled { get; set; }
 
         public string State { get; set; }
+    }
+
+    public class ObjectIdConverter : JsonConverter
+    {
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is ObjectId)
+            {
+                var objectId = (ObjectId)value;
+
+                writer.WriteValue(objectId != ObjectId.Empty ? objectId.ToString() : String.Empty);
+            }
+            else
+            {
+                throw new Exception("Expected ObjectId value.");
+            }
+
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var value = (string)reader.Value;
+            return String.IsNullOrEmpty(value) ? ObjectId.Empty : new ObjectId(value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(ObjectId).IsAssignableFrom(objectType);
+            //return true;
+        }
+
+
     }
 }
