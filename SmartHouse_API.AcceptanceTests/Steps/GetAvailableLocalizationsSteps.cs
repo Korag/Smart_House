4bc69be4d107b076dd401e38f0120f9450fb15e5
khareza <kml.hareza@gmail.com>
2019-06-03 11:18:30 +0200

    Fixed acceptance tests

--- SmartHouse_API.AcceptanceTests/Steps/GetAvailableLocalizationsSteps.cs
+++ SmartHouse_API.AcceptanceTests/Steps/GetAvailableLocalizationsSteps.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using NUnit.Framework;
+using SmartHouse_API.Models;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -8,18 +9,18 @@ using TechTalk.SpecFlow;
 
 namespace SmartHouse_API.AcceptanceTests.Steps
 {
-    [Binding]
+    [Binding, Scope(Tag = "mytag2")]
     public class GetAvailableLocalizationsSteps
     {
         private HttpClient _client = new HttpClient();
         private HttpResponseMessage _responseContent;
-        private List<string> _result;
+        private List<Localization> _result;
 
         [When(@"Client sent get request in order to get available localization")]
         public void WhenClientSentGetRequestInOrderToGetAvailableLocalization()
         {
             _responseContent = _client.GetAsync("https://smarthouseapii.azurewebsites.net/api/GetAvailableLocalizations").Result;
-            _result = JsonConvert.DeserializeObject<List<string>>(_responseContent.Content.ReadAsStringAsync().Result);
+            _result = JsonConvert.DeserializeObject<List<Localization>>(_responseContent.Content.ReadAsStringAsync().Result);
         }
 
         [Then(@"The client should not get empty list")]
@@ -32,7 +33,7 @@ namespace SmartHouse_API.AcceptanceTests.Steps
         [Then(@"The client should get list of available localization")]
         public void ThenTheClientShouldGetListOfAvailableLocalization()
         {
-            Assert.That(_result, Is.TypeOf<List<string>>());
+            Assert.That(_result, Is.TypeOf<List<Localization>>());
         }
 
     }
