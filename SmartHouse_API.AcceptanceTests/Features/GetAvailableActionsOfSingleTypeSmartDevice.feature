f14086fe3e3068d2f431dfd05ec87a32f65c38d9
unknown <kml.hareza@gmail.com>
2019-06-09 20:32:33 +0200

    Added acceptance test that checks if request returns types of smart devices and its avaiable actions

---
+++ SmartHouse_API.AcceptanceTests/Features/GetAvailableActionsOfSingleTypeSmartDevice.feature
@@ -0,0 +1,10 @@
+﻿Feature: GetAvailableActionsOfSingleTypeSmartDevice
+	In order to get available actions of single type smart device
+	Client should send get request to web API
+	As a result of this web request client gets all available actions of single type smart device
+
+@GetAvailableActionsOfSingleTypeSmartDevice
+Scenario: Get available actions of single type smart device
+	When Client sent get request in order to get available actions of single type smart device
+	Then The client should not get empty list
+	Then The client should get list of actions of type
---
+++ SmartHouse_API.AcceptanceTests/Steps/GetAvailableActionsOfSingleTypeSmartDeviceSteps.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using SmartHouse_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SmartHouse_API.AcceptanceTests.Steps
+{
+
+    [Binding, Scope(Tag = "GetAvailableActionsOfSingleTypeSmartDevice")]
+
+    public class GetAvailableActionsOfSingleTypeSmartDeviceSteps
+    {
+        private HttpClient _client = new HttpClient();
+        private HttpResponseMessage _responseContent;
+        private List<TypeActions> _result;
+
+        [When(@"Client sent get request in order to get available actions of single type smart device")]
+        public void WhenClientSentGetRequestInOrderToGetAvailableActionsOfSingleTypeSmartDevice()
+        {
+            _responseContent = _client.GetAsync($"https://smarthouseapii.azurewebsites.net/api/GetTypesOfSmartDevicesWithAvailableActions").Result;
+            _result = JsonConvert.DeserializeObject<List<TypeActions>>(_responseContent.Content.ReadAsStringAsync().Result);
+        }
+
+        [Then(@"The client should not get empty list")]
+        public void ThenTheClientShouldNotGetEmptyList()
+        {
+            Assert.That(_result, Is.Not.Empty);
+        }
+
+        [Then(@"The client should get list of actions of type")]
+        public void ThenTheClientShouldGetListOfActionsOfType()
+        {
+            Assert.That(_result, Is.TypeOf<List<TypeActions>>());
+        }
+
+
+    }
+}
