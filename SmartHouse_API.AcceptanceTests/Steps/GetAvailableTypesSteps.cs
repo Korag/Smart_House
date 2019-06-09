a2e8856bd620d92df99449da1de33aefd3a2275c
unknown <kml.hareza@gmail.com>
2019-06-09 20:52:08 +0200

    Added acceptance test that checks if request returns smart devices types

---
+++ SmartHouse_API.AcceptanceTests/Steps/GetAvailableTypesSteps.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SmartHouse_API.AcceptanceTests.Steps
+{
+    [Binding, Scope(Tag = "GetAvailableTypes")]
+
+    public class GetAvailableTypesSteps
+    {
+        private HttpClient _client = new HttpClient();
+        private HttpResponseMessage _responseContent;
+        private List<string> _result;
+
+        [When(@"Client sent get request in order to get available smart devices types")]
+        public void WhenClientSentGetRequestInOrderToGetAvailableSmartDevicesTypes()
+        {
+            _responseContent = _client.GetAsync($"https://smarthouseapii.azurewebsites.net/api/GetAvailableTypes").Result;
+            _result = JsonConvert.DeserializeObject<List<string>>(_responseContent.Content.ReadAsStringAsync().Result);
+        }
+
+        [Then(@"The client should not get empty list")]
+        public void ThenTheClientShouldNotGetEmptyList()
+        {
+            Assert.That(_result, Is.Not.Empty);
+        }
+
+    }
+}
