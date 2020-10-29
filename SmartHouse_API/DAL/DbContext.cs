2925887bfd2b3e823ce1f1a5c7cd370da1c0c339
Łukasz Czepielik <koragrex@gmail.com>
2020-10-29 22:34:20 +0100

    Changed database engine from mlab to Mongo Atlas.

--- SmartHouse_API/DAL/DbContext.cs
+++ SmartHouse_API/DAL/DbContext.cs
@@ -9,12 +9,11 @@ namespace SmartHouse_API.DAL
 
         public DbContext()
         {
-            string _user = "Admin";
-            string _password = "admin123";
+            string _user = "OpenUser";
+            string _password = "FLbTnwrUSZ";
             string _database = "smarthouse_swagger";
 
-            string _connectionstring = $"mongodb://{_user}:{_password}@ds038547.mlab.com:38547/{_database}";
-
+            string _connectionstring = $"mongodb + srv://{_user}:{_password}@mongodbcluster.wqayz.azure.mongodb.net/{_database}?retryWrites=true&w=majority";
             _mongoClient = new MongoClient(_connectionstring);
             db = _mongoClient.GetDatabase(_database);
         }
